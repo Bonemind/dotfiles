33b1d05f2314ee6ec94bc3751a2d8fd1333ca1e9
Subhi Dweik <subhime@gmail.com>
2019-06-21 11:19:01 +0200

    Different background, monitor name and res change, generic startup script

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,12 +1,20 @@
 #!/bin/env bash
 # xrandr --output eDP1 --scale 1x1 --pos 0x0
-# xrandr --output DP2 --scale 2x2 --mode 1920x1080 --fb 7680x2160 --pos 3840x0
-# xrandr --output eDP1 --auto --output DP2 --mode "3840x2160_fixed" --left-of eDP1
+#xrandr --output DP-2 --scale 2x2 --mode 1920x1080 --fb 7680x2160 --pos 3840x0
+# xrandr --output eDP1 --auto --output DP-2 --mode "3840x2160_fixed" --left-of eDP1
+EXTERNAL=DP-1
+INTERNAL=eDP-1
+PRESENTATION=DP-2
 
-if xrandr | grep "DP1 connected 3840"; then
-  	xrandr --output eDP1 --auto --pos 0x0 --fb 3840x2160 --output DP1 --off
-elif xrandr | grep "DP1 connected"; then
-	xrandr --output DP1 --scale 2x2 --mode 1920x1200 --fb 7680x2400 --pos 0x0 && xrandr --output eDP1 --scale 1x1 --pos 3840x0
+if xrandr | grep "$EXTERNAL disconnected"; then
+	xrandr --output "$EXTERNAL" --off --output "$INTERNAL" --auto
 else
-  	xrandr --output eDP1 --auto --pos 0x0 --fb 3840x2160 --output DP1 --off
+	xrandr --output $EXTERNAL --scale 2x2 --mode 1920x1200 --fb 7680x2400 --pos 0x0 && xrandr --output $INTERNAL --scale 1x1 --pos 3840x0
 fi
+
+# if xrandr | grep "DP1 connected 3840"; then
+#   	xrandr --output eDP1 --auto --pos 0x0 --fb 3840x2160 --output DP1 --off
+# elif xrandr | grep "DP1 connected"; then
+# else
+#   	xrandr --output eDP1 --auto --pos 0x0 --fb 3840x2160 --output DP1 --off
+# fi
---
+++ scripts/startup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# i3-msg exec "lilyterm --title scratchlilyterm -e offlineimap"
+# i3-msg exec --no-startup-id "run_once nm-applet"
+# # < various utility scripts are also run>
+# i3-msg exec iceweasel &
+# i3-msg exec claws-mail &
+i3-msg exec optirun /opt/dynalist/dynalist &
+
+sleep 5
+i3-msg '[ class="Dynalist" ] move scratchpad'
